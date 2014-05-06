a56dfed10070156e3d610291d37455a7ab3cb14e
Kevin Reedy <kevinreedy@gmail.com>
2014-05-05 20:52:20 -0500

    fix PATH issues in tests

--- test/integration/default/bats/consul.bats
+++ test/integration/default/bats/consul.bats
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export PATH=$PATH:/usr/local/bin
 @test "consul is installed and in the PATH" {
     which consul
 }
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -2,3 +2,7 @@ require 'serverspec'
 
 include SpecInfra::Helper::Exec
 include SpecInfra::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.path = '/usr/local/bin:/sbin:/bin:/usr/bin'
+end
