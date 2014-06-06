eeaf1ffad9c24f8b3697e4f5696d74f3bd63c88a
Bernhard K. Weisshuhn <bkw@codingforce.com>
2014-06-06 17:44:19 +0200

    add tests for existence of ui

---
+++ test/integration/ui/serverspec/localhost/consul_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe file('/var/lib/consul/ui/index.html') do
+    it { should be_file }
+end
+
+describe command('wget -q -O- http://0.0.0.0:8500/ui/index.html') do
+  it { should return_exit_status 0 }
+  its(:stdout) { should == File.read('/var/lib/consul/ui/index.html') }
+end
+
