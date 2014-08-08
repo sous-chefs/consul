2c4ddf71ad00b0959c65448aaa1b97275ac8e248
Romesh McCullough <rmccullough@turbine.com>
2014-08-07 22:36:22 -0400

    add interface tests

--- test/integration/ui/serverspec/localhost/consul_spec.rb
+++ test/integration/ui/serverspec/localhost/consul_spec.rb
@@ -4,7 +4,8 @@ describe file('/var/lib/consul/ui/index.html') do
   it { should be_file }
 end
 
-describe command('wget -q -O- http://0.0.0.0:8500/ui/index.html') do
+eth0_ip = command("/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'").stdout.strip
+describe command("wget -q -O- http://#{eth0_ip}:8500/ui/index.html") do
   it { should return_exit_status 0 }
   its(:stdout) { should == File.read('/var/lib/consul/ui/index.html') }
 end
