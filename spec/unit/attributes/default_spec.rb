ed9cb74095e6f9fb36ad93fcfd8709d08889d10a
Jamie Winsor <jamie@vialstudios.com>
2015-02-05 15:56:59 -0800

    remove unit tests which describe exactly what the code describes

--- spec/unit/attributes/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'consul::default' do
-  let(:chef_run) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe)  }
-
-  context 'sets default attributes' do
-    it { expect(chef_run.node.consul.base_url).to eq("https://dl.bintray.com/mitchellh/consul/%{version}.zip") }
-  end
-end
