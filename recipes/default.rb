11adc9c5549f4a090074296c17f4377d182b8910
John Bellone <john.bellone.jr@gmail.com>
2015-02-13 21:05:46 -0500

    Update default recipe to remove user/group resources.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,25 +5,8 @@
 #
 # Copyright 2014, 2015 Bloomberg Finance L.P.
 #
-user node['consul']['service_user'] do
-  system true
-  home '/dev/null'
-  shell '/bin/false'
-  not_if { username == 'root' }
-end
 
-group node['consul']['service_group'] do
-  system true
-  members node['consul']['service_user']
-  not_if { group_name == 'root' }
-end
-
-consul_config node['consul']['config_dir'] do
-  run_user node['consul']['service_user']
-  run_group node['consul']['service_group']
-end
-
-consul_client Chef::Consul.install_path(node) do
+client = consul_client Chef::Consul.install_path(node) do
   filename Chef::Consul.remote_filename(node)
   url Chef::Consul.remote_url(node)
   checksum Chef::Consul.remote_checksum(node)
@@ -32,6 +15,14 @@ consul_client Chef::Consul.install_path(node) do
   run_group node['consul']['service_group']
 end
 
+config = consul_config File.join(node['consul']['config_dir'], 'default.json') do
+  run_user client.run_user
+  run_group client.run_group
+end
+
 consul_service 'consul' do
+  run_user client.run_user
+  run_group client.run_group
+
   action :start
 end
