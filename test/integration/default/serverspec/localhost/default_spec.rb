71e8e334f5d43e42acc94dddf4bb84a4e6d47baf
John Bellone <john.bellone.jr@gmail.com>
2015-06-17 05:58:23 -0400

    Removes the poise-runit cookbook after speaking with @coderanger.
    The way that this cookbook is intended to work is for it to be included
    in a [base cookbook][0] or an [environment cookbook][1] so that the
    whole infrastructure built with poise-service uses runit.
    
    [0]: http://blog.vialstudios.com/the-environment-cookbook-pattern/#thebasecookbook
    [1]: http://blog.vialstudios.com/the-environment-cookbook-pattern/#theenvironmentcookbook

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -10,14 +10,6 @@ describe service('consul') do
   it { should be_running }
 end
 
-describe file('/etc/consul.d') do
-  it { should be_directory }
-end
-
-describe file('/var/lib/consul') do
-  it { should be_directory }
-end
-
 [8300, 8400, 8500, 8600].each do |p|
   describe port(p) do
     it { should be_listening }
@@ -35,17 +27,4 @@ describe 'config file attributes' do
   context command 'consul members -detailed' do
     its(:stdout) { should match %r{\bdc=FortMeade\b}i }
   end
-end
-
-eth0_ip = command("/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'").stdout.strip
-describe command("grep #{eth0_ip} /etc/consul.d/default.json") do
-  its(:exit_status) { should eq 0 }
-  # bind_addr should only be in the config if node[:consul][:bind_addr] is set
-  its(:stdout) { should match %r{"bind_addr":\s"#{eth0_ip}"}}
-  its(:stdout) { should match %r{"advertise_addr":\s"#{eth0_ip}"}}
-end
-
-describe command('grep encrypt /etc/consul.d/default.json') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match %r{"encrypt":\s"([^\"]*)"} }
 end
--- test/integration/runit/serverspec/localhost/default_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe service('consul') do
-  it { should be_running.under('runit') }
-  it { should be_enabled }
-end
