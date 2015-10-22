bc385f9e63a7b944d4a2bde9bbf0c63767695d93
Nick LaMuro <nicklamuro@gmail.com>
2015-10-22 15:04:29 -0500

    Install only flag

--- recipes/default.rb
+++ recipes/default.rb
@@ -42,5 +42,11 @@ consul_service node['consul']['service_name'] do |r|
   config_file config.path
 
   node['consul']['service'].each_pair { |k, v| r.send(k, v) }
-  subscribes :restart, "consul_config[#{config.name}]", :delayed
+  subscribes :restart, "consul_config[#{config.name}]", :delayed if node['consul']['install_only']
+end
+
+if node['consul']['install_only']
+  consul_service node['consul']['service_name'] do
+    action :disable
+  end
 end
