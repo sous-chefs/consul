adc3c2baf7402239cb5385d59580190a82de1d2d
Nick LaMuro <nicklamuro@gmail.com>
2015-10-26 18:13:43 -0500

    recipe/install.rb: fixes copy pasta mistake

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,7 +2,7 @@ poise_service_user node['consul']['service_user'] do
   group node['consul']['service_group']
 end
 
-consul_service node['consul']['service_name'] do |r|
+consul_install node['consul']['service_name'] do |r|
   user node['consul']['service_user']
   group node['consul']['service_group']
   version node['consul']['version']
