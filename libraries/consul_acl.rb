889132ae8ee350ba57cfe4c32a3456863ff5cc08
Brian Clark <brian.clark@steadyserv.com>
2015-12-13 17:41:50 -0500

    general cleanup; unfuse poise resource/provider

--- libraries/consul_acl.rb
+++ libraries/consul_acl.rb
@@ -8,10 +8,11 @@ require 'poise'
 
 module ConsulCookbook
   module Resource
-    # @since 1.0.0
+    # Resource for managing  Consul ACLs.
     class ConsulAcl < Chef::Resource
-      include Poise(fused: true)
+      include Poise
       provides(:consul_acl)
+      actions(:create, :delete)
       default_action(:create)
 
       # @!attribute url
@@ -38,6 +39,36 @@ module ConsulCookbook
       # @return [String]
       attribute(:rules, kind_of: String, default: '')
 
+      def to_acl
+        { 'ID' => id, 'Type' => type, 'Name' => acl_name, 'Rules' => rules }
+      end
+    end
+  end
+
+  module Provider
+    # Provider for managing  Consul ACLs.
+    class ConsulAcl < Chef::Provider
+      include Poise
+      provides(:consul_acl)
+
+      def action_create
+        configure_diplomat
+        unless up_to_date?
+          Diplomat::Acl.create(new_resource.to_acl)
+          new_resource.updated_by_last_action(true)
+        end
+      end
+
+      def action_delete
+        configure_diplomat
+        unless Diplomat::Acl.info(new_resource.id).empty?
+          Diplomat::Acl.destroy(new_resource.id)
+          new_resource.updated_by_last_action(true)
+        end
+      end
+
+      protected
+
       def configure_diplomat
         begin
           require 'diplomat'
@@ -46,42 +77,17 @@ module ConsulCookbook
                               'include recipe[consul::client_gem] to install.'
         end
         Diplomat.configure do |config|
-          config.url = url
-          config.acl_token = auth_token
+          config.url = new_resource.url
+          config.acl_token = new_resource.auth_token
           config.options = { request: { timeout: 10 } }
         end
       end
 
-      def acl_definition
-        acl = {}
-        acl['ID'] = id
-        acl['Type'] = type
-        acl['Name'] = acl_name
-        acl['Rules'] = rules
-        acl
-      end
-
       def up_to_date?
-        old_acl = Diplomat::Acl.info(acl_definition['ID']).first
+        old_acl = Diplomat::Acl.info(new_resource.to_acl['ID']).first
         return false if old_acl.nil?
         old_acl.select! { |k, _v| %w(ID Type Name Rules).include?(k) }
-        old_acl == acl_definition
-      end
-
-      action(:create) do
-        new_resource.configure_diplomat
-        unless new_resource.up_to_date?
-          Diplomat::Acl.create(new_resource.acl_definition)
-          new_resource.updated_by_last_action(true)
-        end
-      end
-
-      action(:delete) do
-        new_resource.configure_diplomat
-        unless Diplomat::Acl.info(new_resource.id).empty?
-          Diplomat::Acl.destroy(new_resource.id)
-          new_resource.updated_by_last_action(true)
-        end
+        old_acl == new_resource.to_acl
       end
     end
   end
