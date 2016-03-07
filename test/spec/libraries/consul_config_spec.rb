ea2fe47769a76c0bb22a1a3d15f27e3e1932c4aa
John Bellone <jbellone@bellone.us>
2016-03-07 16:54:57 -0500

    Remove TLS certificate management from this policy.
    I am deferring the management of TLS certificates to wrapper
    cookbooks. This closes #247.

--- test/spec/libraries/consul_config_spec.rb
+++ test/spec/libraries/consul_config_spec.rb
@@ -8,7 +8,6 @@ describe ConsulCookbook::Resource::ConsulConfig do
   before do
     recipe = double('Chef::Recipe')
     allow_any_instance_of(Chef::RunContext).to receive(:include_recipe).and_return([recipe])
-    allow_any_instance_of(Chef::Provider).to receive(:chef_vault_item) { { 'ca_certificate' => 'foo', 'certificate' => 'bar', 'private_key' => 'baz' }  }
   end
 
   context 'sets options directly' do
@@ -27,50 +26,6 @@ describe ConsulCookbook::Resource::ConsulConfig do
   "verify_outgoing": false
 }
 EOH
-  end
-
-  context 'manages certificates' do
-    recipe do
-      consul_config '/etc/consul/default.json' do
-        key_file '/etc/consul/ssl/private/consul.key'
-        ca_file '/etc/consul/ssl/CA/ca.crt'
-        cert_file '/etc/consul/ssl/certs/consul.crt'
-        verify_incoming true
-        verify_outgoing true
-      end
-    end
-
-    it { is_expected.to create_directory('/etc/consul/ssl/CA') }
-    it { is_expected.to create_directory('/etc/consul/ssl/certs') }
-    it { is_expected.to create_directory('/etc/consul/ssl/private') }
-
-    it do
-      is_expected.to create_file('/etc/consul/ssl/CA/ca.crt')
-      .with(content: 'foo')
-      .with(owner: 'consul')
-      .with(group: 'consul')
-      .with(mode: '0644')
-    end
-
-    it do
-      is_expected.to create_file('/etc/consul/ssl/certs/consul.crt')
-      .with(content: 'bar')
-      .with(owner: 'consul')
-      .with(group: 'consul')
-      .with(mode: '0644')
-    end
-
-    it do
-      is_expected.to create_file('/etc/consul/ssl/private/consul.key')
-      .with(content: 'baz')
-      .with(sensitive: true)
-      .with(owner: 'consul')
-      .with(group: 'consul')
-      .with(mode: '0640')
-    end
-
-    it { is_expected.to create_directory('/etc/consul') }
-    it { is_expected.to create_file('/etc/consul/default.json') }
   end
 
   context 'deletes configuration' do
