2dded6e24a6b2a6351f0ec54b4410cb971f1b5b4
Demitri Swan <demitri@moz.com>
2016-04-11 14:03:06 -0700

    Got rake travis working locally. Trying in travis

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -3,5 +3,5 @@ require 'chefspec/berkshelf'
 require 'poise_boiler/spec_helper'
 require_relative '../../libraries/helpers'
 
-# require 'codeclimate-test-reporter'
-# CodeClimate::TestReporter.start
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
