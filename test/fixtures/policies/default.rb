8e869d60f2b95d628920825c059489680c036c9e
John Bellone <jbellone@bellone.us>
2016-07-10 13:57:03 -0500

    Update travis.yml configuration to run unit tests.

--- test/fixtures/policies/default.rb
+++ test/fixtures/policies/default.rb
@@ -2,7 +2,7 @@ name 'consul'
 default_source :community
 cookbook 'consul', path: '.'
 run_list 'consul::default'
-named_run_list :redhat, 'redhat::default', 'consul::default', run_list
-named_run_list :ubuntu, 'ubuntu::default', 'consul::default', run_list
+named_run_list :centos, 'yum::default', 'consul::default', run_list
+named_run_list :debian, 'apt::default', 'consul::default', run_list
 override['consul']['config']['bootstrap'] = true
 override['consul']['config']['server'] = true
