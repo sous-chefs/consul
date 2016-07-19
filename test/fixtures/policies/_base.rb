eb7f3f6ca1a1ac9952f6a20e028875fc0f79cd26
John Bellone <jbellone@bellone.us>
2016-07-19 05:47:48 -0400

    Update travis.yml configuration for kitchen-dokken.

--- test/fixtures/policies/_base.rb
+++ test/fixtures/policies/_base.rb
@@ -2,7 +2,7 @@ default_source :community
 default_source :chef_repo, '..'
 cookbook 'consul', path: '../../..'
 run_list 'consul::default', "consul_spec::#{name}"
-named_run_list :rhel, 'yum::default', run_list
+named_run_list :centos, 'yum::default', run_list
 named_run_list :debian, 'apt::default', run_list
 named_run_list :freebsd, 'freebsd::default', run_list
 named_run_list :windows, 'windows::default', run_list
