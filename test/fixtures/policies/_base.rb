b979d6b1ff060195e40fea74a127624d02d8f4e9
John Bellone <jbellone@bellone.us>
2016-07-24 20:53:34 -0400

    Add sudo default recipe to fix /etc/sudoers

--- test/fixtures/policies/_base.rb
+++ test/fixtures/policies/_base.rb
@@ -1,7 +1,7 @@
 default_source :community
 default_source :chef_repo, '..'
 cookbook 'consul', path: '../../..'
-run_list 'consul::default', "consul_spec::#{name}"
+run_list 'sudo::default', 'consul::default', "consul_spec::#{name}"
 named_run_list :centos, 'yum::default', 'yum-centos::default', run_list
 named_run_list :debian, 'apt::default', run_list
 named_run_list :freebsd, 'freebsd::default', run_list
