4e02836032b28b80e1ac407b4728117f2c2a6d39
Graham Davison <gdavison@paybyphone.com>
2016-09-16 14:24:59 -0700

    Adding sudo back to centos run_list

--- test/fixtures/policies/_base.rb
+++ test/fixtures/policies/_base.rb
@@ -2,7 +2,7 @@ default_source :community
 default_source :chef_repo, '..'
 cookbook 'consul', path: '../../..'
 run_list 'consul::default', "consul_spec::#{name}"
-named_run_list :centos, 'yum::default', 'yum-centos::default',run_list
+named_run_list :centos, 'yum::default', 'yum-centos::default', 'sudo::default',run_list
 named_run_list :debian, 'apt::default', run_list
 named_run_list :freebsd, 'freebsd::default', 'sudo::default',  run_list
 named_run_list :windows, 'windows::default', run_list
