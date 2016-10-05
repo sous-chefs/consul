8235de26533c570b2eb69bc29b31bdf6ab082f2f
Tim Petter <tim@timpetter.de>
2016-10-05 09:58:48 +0200

    Use config user/group for consul_definition instead of hard coded string

--- test/spec/libraries/consul_definition_spec.rb
+++ test/spec/libraries/consul_definition_spec.rb
@@ -6,6 +6,10 @@ describe ConsulCookbook::Resource::ConsulDefinition do
   let(:chefspec_options) { {platform: 'ubuntu', version: '14.04'} }
   before do
     default_attributes['consul'] = {
+      'config' => {
+        'owner' => 'consul',
+        'group' => 'consul'
+      },
       'service' => {
         'config_dir' => '/etc/consul/conf.d'
         }
@@ -50,6 +54,42 @@ describe ConsulCookbook::Resource::ConsulDefinition do
     it do
       is_expected.to create_file('/etc/consul/conf.d/redis.json')
       .with(user: 'root', group: 'consul', mode: '0640')
+      .with(content: JSON.pretty_generate(
+        service: {
+          name: 'myredis',
+          tags: ['master'],
+          address: '127.0.0.1',
+          port: 6379,
+          interval: '10s'
+        }
+      ))
+    end
+  end
+
+  context 'service definition with owner and group from node config' do
+    before do
+      default_attributes['consul'] = {
+        'config' => {
+          'owner' => 'root',
+          'group' => 'root'
+        },
+        'service' => {
+          'config_dir' => '/etc/consul/conf.d'
+          }
+        }
+    end
+
+    recipe do
+      consul_definition 'redis' do
+        type 'service'
+        parameters(name: 'myredis', tags: %w{master}, address: '127.0.0.1', port: 6379, interval: '10s')
+      end
+    end
+
+    it { is_expected.to create_directory('/etc/consul/conf.d') }
+    it do
+      is_expected.to create_file('/etc/consul/conf.d/redis.json')
+      .with(user: 'root', group: 'root', mode: '0640')
       .with(content: JSON.pretty_generate(
         service: {
           name: 'myredis',
