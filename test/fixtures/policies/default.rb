c5979ab245b4eb889aaa06089d082ddcf9e8a0d3
Mikhail Zholobov <legal90@gmail.com>
2016-12-15 13:31:15 +0200

    Enable passwordless sudo for test user on RHEL/CentOS

--- test/fixtures/policies/default.rb
+++ test/fixtures/policies/default.rb
@@ -1,9 +1,12 @@
 name 'default'
-default_source :community
+default_source :supermarket
 default_source :chef_repo, '..'
 cookbook 'consul', path: '../../..'
 run_list 'consul::default', "consul_spec::#{name}"
-named_run_list :centos, 'yum::default', 'yum-centos::default', 'sudo::default', run_list
+named_run_list :centos, 'sudo::default', run_list
 named_run_list :debian, 'apt::default', run_list
 named_run_list :freebsd, 'freebsd::default', 'sudo::default', run_list
 named_run_list :windows, 'windows::default', run_list
+
+default['authorization']['sudo']['users'] = %w(kitchen vagrant)
+default['authorization']['sudo']['passwordless'] = true
