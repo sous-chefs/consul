65a12ac9998017dd2f5ac934611788036073a9a5
Mikhail Zholobov <legal90@gmail.com>
2016-12-15 13:29:08 +0200

    test: Use a single policy for all test suites

--- test/fixtures/policies/client.rb
+++
@@ -1,2 +0,0 @@
-name 'client'
-instance_eval(IO.read(File.expand_path('../_base.rb', __FILE__)))
--- test/fixtures/policies/default.rb
+++ test/fixtures/policies/default.rb
@@ -1,2 +1,9 @@
 name 'default'
-instance_eval(IO.read(File.expand_path('../_base.rb', __FILE__)))
+default_source :community
+default_source :chef_repo, '..'
+cookbook 'consul', path: '../../..'
+run_list 'consul::default', "consul_spec::#{name}"
+named_run_list :centos, 'yum::default', 'yum-centos::default', 'sudo::default', run_list
+named_run_list :debian, 'apt::default', run_list
+named_run_list :freebsd, 'freebsd::default', 'sudo::default', run_list
+named_run_list :windows, 'windows::default', run_list
