b18f4eee23c77fd95e2a00ed92f94f35a0499c61
Mikhail Zholobov <legal90@gmail.com>
2017-04-23 18:01:54 +0200

    Disable CodeClimate reporter

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -2,6 +2,3 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'poise_boiler/spec_helper'
 require_relative '../../libraries/helpers'
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
