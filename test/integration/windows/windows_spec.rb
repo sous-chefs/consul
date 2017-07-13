7872fa0f3b8ae5a0a83c63fb415557c8fdec728a
Graham Davison <gdavison@paybyphone.com>
2017-07-12 23:44:57 -0700

    Rearranged file path variables in tests

--- test/integration/windows/windows_spec.rb
+++ test/integration/windows/windows_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../spec_helper'
 
-consul_executable = "C:\\Program\ Files\\consul\\#{consul_version}\\consul.exe"
+consul_root       = 'C:\Program Files\consul'
+
+consul_executable = File.join(consul_root, consul_version, 'consul.exe')
+symlink_path      = File.join(consul_root, 'consul.exe')
 consul_command    = "& '#{consul_executable}'"
 
 config_file = 'C:\Program Files\consul\consul.json'
@@ -30,19 +33,18 @@ describe command("#{consul_command} members -detailed -token=doublesecret") do
   its(:stdout) { should include 'dc=fortmeade' }
 end
 
-symlink_path = 'C:\Program Files\consul\consul.exe'
 describe file(symlink_path) do
   it { should be_file }
 end
 # Because we can't use be_symlink for Windows
 describe command("(Get-Item \"#{symlink_path}\").Attributes -match 'ReparsePoint'") do
-  its(:exit_status) { should eq 0 }
+  its(:exit_status)   { should eq 0      }
   its('stdout.strip') { should eq 'True' }
 end
 
 # Commented out, because reloading the path doesn't get picked up on the first run
 # describe os_env('PATH') do
-#   its('split') { should include 'C:\Program Files\consul'}
+#   its('split') { should include consul_root }
 # end
 
 describe file(config_file) do
