5b8c2e10499abea8a19d574fad582d1a6fe5345b
Baptiste Courtois <b.courtois@criteo.com>
2017-08-08 22:51:16 +0200

    Leverage nssm 4.0.0 and remove obsolete code
    NSSM cookbook version 4 improved its parameter handling and idempotency.
    No need to do weird quoting, this is a small breaking change!

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -46,81 +46,9 @@ module ConsulCookbook
 
     def command(config_file, config_dir)
       if windows?
-        %(agent -config-file="""#{config_file}""" -config-dir="""#{config_dir}""")
+        %(agent -config-file="#{config_file}" -config-dir="#{config_dir}")
       else
         "/usr/local/bin/consul agent -config-file=#{config_file} -config-dir=#{config_dir}"
-      end
-    end
-
-    def nssm_exe
-      "#{node['nssm']['install_location']}\\nssm.exe"
-    end
-
-    def nssm_params
-      %w(Application
-         AppParameters
-         AppDirectory
-         AppExit
-         AppAffinity
-         AppEnvironment
-         AppEnvironmentExtra
-         AppNoConsole
-         AppPriority
-         AppRestartDelay
-         AppStdin
-         AppStdinShareMode
-         AppStdinCreationDisposition
-         AppStdinFlagsAndAttributes
-         AppStdout
-         AppStdoutShareMode
-         AppStdoutCreationDisposition
-         AppStdoutFlagsAndAttributes
-         AppStderr
-         AppStderrShareMode
-         AppStderrCreationDisposition
-         AppStderrFlagsAndAttributes
-         AppStopMethodSkip
-         AppStopMethodConsole
-         AppStopMethodWindow
-         AppStopMethodThreads
-         AppThrottle
-         AppRotateFiles
-         AppRotateOnline
-         AppRotateSeconds
-         AppRotateBytes
-         AppRotateBytesHigh
-         DependOnGroup
-         DependOnService
-         Description
-         DisplayName
-         ImagePath
-         ObjectName
-         Name
-         Start
-         Type)
-    end
-
-    def nssm_service_installed?
-      # 1 is command not found
-      # 3 is service not found
-      exit_code = shell_out!(%("#{nssm_exe}" status consul), returns: [0, 1, 3]).exitstatus
-      exit_code == 0
-    end
-
-    def nssm_service_status?(expected_status)
-      expected_status.include? shell_out!(%("#{nssm_exe}" status consul), returns: [0]).stdout.delete("\0").strip
-    end
-
-    # Returns a hash of mismatched params
-    def check_nssm_params(extra = {})
-      params = node['consul']['service']['nssm_params'].merge extra
-      # nssm can only get certain values
-      to_check = params.select { |k, _v| nssm_params.include? k.to_s }
-      to_check.each.each_with_object({}) do |(k, v), mismatch|
-        # shell_out! returns values with null bytes, need to delete them before we evaluate
-        unless shell_out!(%("#{nssm_exe}" get consul #{k}), returns: [0]).stdout.delete("\0").strip.eql? v.to_s
-          mismatch[k] = v
-        end
       end
     end
   end
