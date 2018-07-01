2a2d09523cbf07e26a8988e02bd549a16721eac2
Pierre Souchay <p.souchay@criteo.com>
2018-07-01 19:12:29 +0200

    Apply new attribute discovery_max_stale only for Consul 1.x

--- libraries/consul_config_v0.rb
+++ libraries/consul_config_v0.rb
@@ -72,7 +72,6 @@ module ConsulCookbook
       attribute(:disable_remote_exec, equal_to: [true, false])
       attribute(:disable_update_check, equal_to: [true, false])
       attribute(:discard_check_output, equal_to: [true, false])
-      attribute(:discovery_max_stale, kind_of: String)
       attribute(:dns_config, kind_of: [Hash, Mash])
       attribute(:domain, kind_of: String)
       attribute(:enable_acl_replication, equal_to: [true, false])
@@ -237,7 +236,6 @@ module ConsulCookbook
           watches
         )
 
-        for_keeps << %i(discovery_max_stale) if node['consul']['version'] > '1.0.6'
         for_keeps << %i(bootstrap bootstrap_expect) if server
         for_keeps << %i(ca_file ca_path cert_file key_file) if tls?
         for_keeps = for_keeps.flatten
