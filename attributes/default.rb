2c7490334001881b7ea2f17a35edd6e52b2f60dc
Anthony Scalisi <scalisi.a@gmail.com>
2019-05-02 15:44:14 -0700

    allow overriding diplomat gem name for forks

--- attributes/default.rb
+++ attributes/default.rb
@@ -27,6 +27,7 @@ default['consul']['config']['ports'] = {
   'server'   => 8300,
 }
 
+default['consul']['diplomat_gem'] = 'diplomat'
 default['consul']['diplomat_version'] = nil
 
 default['consul']['service']['config_dir'] = join_path config_prefix_path, 'conf.d'
