372ecb788b19235312b1140f82531d1c874e91fa
Dan Webb <dan.webb@damacus.io>
2019-08-19 20:40:07 +0100

    Auto correct using cookstyle

--- metadata.rb
+++ metadata.rb
@@ -17,7 +17,7 @@ supports 'arch'
 supports 'windows'
 
 # build-essential is obsolete in chef 14+
-depends 'build-essential' # ~FC121
+# ~FC121
 depends 'nssm', '>= 4.0.0'
 depends 'golang'
 depends 'poise', '~> 2.2'
