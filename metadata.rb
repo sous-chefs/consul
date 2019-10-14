4f747978b0d0590f3723c1be93f4706667729a82
dtaylor <dtaylor@roblox.com>
2019-10-14 09:56:29 -0400

    Support for Windows using Chef 14

--- metadata.rb
+++ metadata.rb
@@ -17,7 +17,7 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache-2.0'
 description 'Application cookbook which installs and configures Consul.'
 long_description 'Application cookbook which installs and configures Consul.'
-version '9003.1.9'
+version '9003.1.10'
 
 recipe 'consul::default', 'Installs and configures the Consul service.'
 recipe 'consul::client_gem', 'Installs the Consul Ruby client as a gem.'
@@ -27,7 +27,6 @@ supports 'redhat', '>= 6.4'
 supports 'ubuntu', '>= 12.04'
 supports 'solaris2'
 supports 'arch'
->>>>>>> Do not elide key/cert/ca information if tls verification is disabled
 supports 'windows'
 
 depends 'build-essential', '>= 5.0.0' # cookstyle: disable ChefModernize/UnnecessaryDependsChef14
@@ -36,3 +35,9 @@ depends 'golang'
 depends 'poise', '~> 2.2'
 depends 'poise-archive', '~> 1.3'
 depends 'poise-service', '~> 1.4'
+depends 'windows', '>= 3.1'
+
+source_url 'https://github.com/johnbellone/consul-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/johnbellone/consul-cookbook/issues' if respond_to?(:issues_url)
+
+chef_version '>= 12.1' if respond_to?(:chef_version)
