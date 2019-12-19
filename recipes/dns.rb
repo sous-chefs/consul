ee0608eab513221c7c3c66253f63468c0adc01df
Charles Zaffery <czaffery@roblox.com>
2019-12-19 11:32:41 -0800

    restart consul on dns change

--- recipes/dns.rb
+++ recipes/dns.rb
@@ -6,12 +6,13 @@ end
 # Stop and disable dnsmasq
 systemd_unit 'dnsmasq.service' do
    action [:disable, :stop]
+   notifies :restart, 'service[consul]'
 end
 
 # Stop and disable systemd-resolved
 systemd_unit 'systemd-resolved.service' do
     action [:disable, :stop]
-    notifies :reload, 'service[consul]'
+    notifies :restart, 'service[consul]'
 end
 
 # Defined to avoid any issues with poise-*
--- recipes/systemd-resolved.rb
+++
@@ -1,9 +0,0 @@
-# undo stub symlink to standard symlink
-link '/etc/resolv.conf' do
-    to '/run/systemd/resolve/resolv.conf'
-end
-
-# Stop and disable systemd-resolved
-systemd_unit 'systemd-resolved.service' do
-    action [:disable, :stop]
-end
