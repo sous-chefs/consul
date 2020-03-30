7a264a69f05ff2eae08f5fa77fb1460bbba050f9
Ben Kochie <superq@gmail.com>
2020-03-30 10:45:02 +0200

    Update deps and chef
    Update Chef minimum version.
    * Eliminates the need for the windows cookbook.
    * Update Linux testing versions.
    * Set minimum Chef version to 13.4.
    * Update dokken testing to Chef 14.
    
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -18,9 +18,8 @@ depends 'golang'
 depends 'poise', '~> 2.2'
 depends 'poise-archive', '~> 1.3'
 depends 'poise-service', '~> 1.4'
-depends 'windows', '~> 3.1'
 
 source_url 'https://github.com/sous-chefs/consul'
 issues_url 'https://github.com/sous-chefs/consul/issues'
 
-chef_version '>= 12.1'
+chef_version '>= 13.4'
