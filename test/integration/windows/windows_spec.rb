2074cbb4d89a42b13b815f628d59bd0d7add7463
Jonathan Amiez <j.amiez@criteo.com>
2021-10-26 17:51:23 +0200

    Reintroduce spec_helper
    Even with no spec, seems to make CI fail.

--- test/integration/windows/windows_spec.rb
+++ test/integration/windows/windows_spec.rb
@@ -3,7 +3,6 @@ require_relative '../spec_helper'
 consul_root = 'C:\Program Files\consul'
 
 consul_executable = File.join(consul_root, consul_version, 'consul.exe')
-symlink_path      = File.join(consul_root, 'consul.exe')
 consul_command    = "& '#{consul_executable}'"
 
 config_file = 'C:\Program Files\consul\consul.json'
@@ -33,14 +32,16 @@ describe command("#{consul_command} members -detailed -token=doublesecret") do
   its(:stdout) { should include 'dc=fortmeade' }
 end
 
-describe file(symlink_path) do
-  it { should be_file }
-end
+# Symlink creation disabled
+# symlink_path = File.join(consul_root, 'consul.exe')
+# describe file(symlink_path) do
+#   it { should be_file }
+# end
 # Because we can't use be_symlink for Windows
-describe command("(Get-Item \"#{symlink_path}\").Attributes -match 'ReparsePoint'") do
-  its(:exit_status)   { should eq 0      }
-  its('stdout.strip') { should eq 'True' }
-end
+# describe command("(Get-Item \"#{symlink_path}\").Attributes -match 'ReparsePoint'") do
+#   its(:exit_status)   { should eq 0      }
+#   its('stdout.strip') { should eq 'True' }
+# end
 
 # Commented out, because reloading the path doesn't get picked up on the first run
 # describe os_env('PATH') do
